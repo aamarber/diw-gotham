8810369ea15b9b17d3348a31d7c6ddeebefbd40e
melocotoneroh <aamarber@gmail.com>
2022-12-09 11:44:28 +0100

    Everything adapted to Bootstrap 5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function () {
-    // activación de tooltips
-    $('[data-toggle="tooltip"]').tooltip();
-});
+(() => {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+})();
